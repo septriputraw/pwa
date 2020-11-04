303a4f37c9d182106a0da7b114a7e28a35574440
septri <09021381621129@students.ilkom.unsri.ac.id>
2020-11-04 16:19:09 +0700

    change icon

--- service-worker.js
+++ service-worker.js
@@ -98,43 +98,6 @@ workbox.routing.registerRoute(
     ],
   })
 );
- 
-/*self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener("fetch", (event) => {
-    var base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-      event.respondWith(
-        caches.open(CACHE_NAME).then((cache) => fetch(event.request).then((response) => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          }))
-      );
-    } else {
-      event.respondWith(
-        caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-          return response || fetch (event.request);
-        })
-      )
-    }
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-      caches.keys().then((cacheNames) => Promise.all(
-          cacheNames.map((cacheName) => {
-              if (cacheName != CACHE_NAME) {
-                console.log("ServiceWorker: cache " + cacheName + " dihapus");
-                return caches.delete(cacheName);
-              }
-            })
-        ))
-    );
-});*/
 
 //push
 self.addEventListener('push', (event) => {
